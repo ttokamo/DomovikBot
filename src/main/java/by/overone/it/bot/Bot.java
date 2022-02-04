fe6284962dbe323edd112382451df268a626ebee
Vsevolod24 <vsevolodkucko@gmail.com>
2022-02-04 14:22:18 +0300

    class Bot оптимизация методов callbackQuery и callbackData в методе Update

--- src/main/java/by/overone/it/bot/Bot.java
+++ src/main/java/by/overone/it/bot/Bot.java
@@ -38,36 +38,59 @@ public class Bot extends TelegramLongPollingBot {
         if (update.hasMessage()) {
             String username = update.getMessage().getFrom().getFirstName();
             String start = update.getMessage().getText();
-//            String getData = update.getCallbackQuery().getData();
             if (start.startsWith("/start")) {
                 execute(sendMessage(update.getMessage().getChatId(), sendInlineKeyBoardMessageMainMenu(),
                         "Привет в нашем телеграм-боте " + username));
             } else if (start.startsWith("/stop")) {
                 execute(sendMessage(update.getMessage().getChatId(), null, "До свидания"));
             }
+        } else if (update.hasCallbackQuery()) {
+            String callback = update.getCallbackQuery().getData();
+            Long chatId = update.getCallbackQuery().getMessage().getChatId();
 
-        } else if (update.getCallbackQuery().getData().equals("Button menu")) {
-            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), BotSecondMenu.sendSecondMenu(),
-                    "Вы нажали на меню"));
+            if (callback.startsWith("Menu")) {
+                execute(sendMessage(chatId, BotSecondMenu.sendSecondMenu(), "Вы нажали на меню"));
 
-        } else if (update.getCallbackQuery().getData().equals("Button registration")) {
-            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), sendInlineKeyBoardMessageMainMenu(),
-                    "Вы выбрали регистрацию"));
+            } else if (callback.equals("Registration")) {
+                execute(sendMessage(chatId, sendInlineKeyBoardMessageMainMenu(), "Вы выбрали регистрацию"));
 
-        } else if (update.getCallbackQuery().getData().equals("Button back")) {
-            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), sendInlineKeyBoardMessageMainMenu(),
-                    "Если вы уверены что хотите выйти кликните сюда -> " + "/stop"));
+            } else if (callback.equals("Exit")) {
+                execute(sendMessage(chatId, sendInlineKeyBoardMessageMainMenu(),
+                        "Если вы уверены что хотите выйти кликните сюда -> " + "/stop"));
 
-        } else if (update.getCallbackQuery().getData().equals("Button baraholka")) {
-            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), BotBaraholkaMenu.sendBaraholka(), "Вы нажали на барахолка"));
+            } else if (callback.equals("Baraholka")) {
+                execute(sendMessage(chatId, BotBaraholkaMenu.sendBaraholka(), "Вы нажали на барахолка"));
 
-        } else if (update.getCallbackQuery().getData().equals("Button JKH")) {
-            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), BotJKHMenu.sendJKHMenu(), "Вы нажали на ЖКХ"));
-        } else if (update.getCallbackQuery().getData().equals("add advert")) {
-            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), null, "Введите ваше имя для, чтобы зарегистрировать Вас"));
+            } else if (callback.equals("JKH")) {
+                execute(sendMessage(chatId, BotJKHMenu.sendJKHMenu(), "Вы нажали на ЖКХ"));
+
+            } else if (callback.equals("add advert")) {
+                execute(sendMessage(chatId, null, "Введите ваше имя для, чтобы зарегистрировать Вас"));
+            }
         }
-
     }
+
+
+//        } else if (update.getCallbackQuery().getData().equals("Menu")) {
+//            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), BotSecondMenu.sendSecondMenu(),
+//                    "Вы нажали на меню"));
+
+//        } else if (update.getCallbackQuery().getData().equals("Button registration")) {
+//            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), sendInlineKeyBoardMessageMainMenu(),
+//                    "Вы выбрали регистрацию"));
+//
+//        } else if (update.getCallbackQuery().getData().equals("Button back")) {
+//            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), sendInlineKeyBoardMessageMainMenu(),
+//                    "Если вы уверены что хотите выйти кликните сюда -> " + "/stop"));
+//
+//        } else if (update.getCallbackQuery().getData().equals("Button baraholka")) {
+//            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), BotBaraholkaMenu.sendBaraholka(), "Вы нажали на барахолка"));
+//
+//        } else if (update.getCallbackQuery().getData().equals("Button JKH")) {
+//            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), BotJKHMenu.sendJKHMenu(), "Вы нажали на ЖКХ"));
+//        } else if (update.getCallbackQuery().getData().equals("add advert")) {
+//            execute(sendMessage(update.getCallbackQuery().getMessage().getChatId(), null, "Введите ваше имя для, чтобы зарегистрировать Вас"));
+//        }
 
 
     //    Главное меню
@@ -76,11 +99,11 @@ public class Bot extends TelegramLongPollingBot {
         InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
 
         inlineKeyboardButton1.setText("Меню");
-        inlineKeyboardButton1.setCallbackData("Button menu");
+        inlineKeyboardButton1.setCallbackData("Menu");
 
         InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
         inlineKeyboardButton2.setText("Регистрация");
-        inlineKeyboardButton2.setCallbackData("Button registration");
+        inlineKeyboardButton2.setCallbackData("Registration");
 
         List<InlineKeyboardButton> keyboardButtonsRaw1 = new ArrayList<>();
         keyboardButtonsRaw1.add(inlineKeyboardButton1);
@@ -88,7 +111,7 @@ public class Bot extends TelegramLongPollingBot {
 
         InlineKeyboardButton inlineKeyboardButton3 = new InlineKeyboardButton();
         inlineKeyboardButton3.setText("Выход");
-        inlineKeyboardButton3.setCallbackData("Button back");
+        inlineKeyboardButton3.setCallbackData("Exit");
 
         List<InlineKeyboardButton> keyboardButtonsRaw2 = new ArrayList<>();
         keyboardButtonsRaw2.add(inlineKeyboardButton3);
