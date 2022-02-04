49b17346570b9dbefc33e1c88d6c6e2a41126435
aleksey <worcers@gmail.com>
2022-02-04 13:01:57 +0300

    Базовые классы для доступа к бд

---
+++ src/main/java/by/overone/it/service/UserService.java
@@ -0,0 +1,25 @@
+package by.overone.it.service;
+
+import by.overone.it.entity.User;
+import by.overone.it.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.getUserByUsername(username);
+    }
+
+    public String getUsername(String username) {
+        return userRepository.getUsername(username);
+    }
+}
