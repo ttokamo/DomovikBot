3311d0205d78902d85a60e091e3faae45831a99c
Vsevolod24 <vsevolodkucko@gmail.com>
2022-02-06 22:47:39 +0300

    Вопросы при регистрации

---
+++ src/main/java/by/overone/it/question/Questions.java
@@ -0,0 +1,21 @@
+package by.overone.it.question;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Questions {
+
+    static String secondName = "Введите фамилию:";
+    static String street = "Ведите улицу:";
+    static String house = "Ведите номер дома:";
+    static String porch = "Ведите номер подъезда:";
+    static String floor = "Ведите номер этажа:";
+    static String flat = "Ведите номер квартиры:";
+    static String telephoneNumber = "Ведите номер телефона:";
+    static String car = "У вас имеется личный автомобиль?";
+    static String carNumber = "Ведите регистрационный номер машины:";
+    static String end = "Анкета заполнена";
+    static String finishMarket = "Ваше объявление успешно зарегистрировано";
+
+}
+
