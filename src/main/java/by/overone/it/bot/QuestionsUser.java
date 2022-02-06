8b2bedeaeaba4a97b0c19032451f76146dba574a
Nahodkin <3477734n@gmail.ru>
2022-02-06 20:46:41 +0300

    Questionsuser (sendMessage3)

--- src/main/java/by/overone/it/bot/QuestionsUser.java
+++ src/main/java/by/overone/it/bot/QuestionsUser.java
@@ -39,42 +39,34 @@ public class QuestionsUser {
             botStatusService.updateBotStatus(chatId, BotStatusEnums.ASK_ABOUT_STREET.toString());
             user.setUsername(username);
             user.setName(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.ASK_ABOUT_STREET.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(street, chatId, false, null);
             botStatusService.updateBotStatus(chatId, BotStatusEnums.ASK_ABOUT_HOUSE.toString());
             user.setStreet(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.ASK_ABOUT_HOUSE.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(house, chatId, false, null);
             botStatusService.updateBotStatus(chatId, BotStatusEnums.ASK_ABOUT_PORCH.toString());
             user.setHouse(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.ASK_ABOUT_PORCH.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(porch, chatId, false, null);
             botStatusService.updateBotStatus(chatId, BotStatusEnums.ASK_ABOUT_FLOOR.toString());
             user.setPorchNumber(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.ASK_ABOUT_FLOOR.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(floor, chatId, false, null);
             botStatusService.updateBotStatus(chatId, BotStatusEnums.ASK_ABOUT_FLAT.toString());
             user.setFloor(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.ASK_ABOUT_FLAT.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(flat, chatId, false, null);
             botStatusService.updateBotStatus(chatId, BotStatusEnums.ASK_ABOUT_TELEPHONE_NUMBER.toString());
             user.setFlat(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.ASK_ABOUT_TELEPHONE_NUMBER.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(telephoneNumber, chatId, false, null);
             botStatusService.updateBotStatus(chatId, BotStatusEnums.ASK_ABOUT_CAR.toString());
             user.setTelephoneNumber(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.ASK_ABOUT_CAR_NUMBER.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(carNumber, chatId, false, null);
             botStatusService.updateBotStatus(chatId, BotStatusEnums.FINISH.toString());
             user.setCarNumber(message);
-            return sendMessage;
         } else if (status.equals(BotStatusEnums.FINISH.toString())) {
             sendMessage = SendMessageConstructor.sendMessage(end, chatId, false, null);
             botStatusService.deleteBotStatusByUsername(chatId);
