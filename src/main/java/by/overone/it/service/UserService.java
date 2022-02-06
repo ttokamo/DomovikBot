a5a5c09f2dd9458470de74eb8775bbe847a147f5
tokamo <worcers@gmail.com>
2022-02-06 15:48:09 +0300

    new service method

--- src/main/java/by/overone/it/service/UserService.java
+++ src/main/java/by/overone/it/service/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
     public String getUsername(String username) {
         return userRepository.getUsername(username);
     }
+
+    public void deleteUserByUsername(String username) {
+        userRepository.deleteUserByUsername(username);
+    }
 }
